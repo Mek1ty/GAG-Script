e1007dddd8d933b575adf2087d8073ea66c1971a
Mek1ty <126418095+Mek1ty@users.noreply.github.com>
2025-06-21 10:34:20 +0300

    Add files via upload

---
+++ bundled.lua
@@ -0,0 +1,55 @@
+local __DARKLUA_BUNDLE_MODULES __DARKLUA_BUNDLE_MODULES={cache={}, load=function(m)if not __DARKLUA_BUNDLE_MODULES.cache[m]then __DARKLUA_BUNDLE_MODULES.cache[m]={c=__DARKLUA_BUNDLE_MODULES[m]()}end return __DARKLUA_BUNDLE_MODULES.cache[m].c end}do function __DARKLUA_BUNDLE_MODULES.a()local Players = game:GetService("Players")
+local VirtualInputManager = game:GetService("VirtualInputManager")
+
+local AutoStart = {}
+
+function AutoStart:WaitAndStart()
+    local playerGui = Players.LocalPlayer:WaitForChild("PlayerGui", 10)
+    if not playerGui then
+        warn("[AutoStart] PlayerGui не найден")
+        return
+    end
+
+    local loadedValue
+    local maxWaitTime = 30
+    local startTime = tick()
+
+    
+    while tick() - startTime < maxWaitTime do
+        local loadedCandidate = playerGui:FindFirstChild("Intro_SCREEN", true)
+            and playerGui.Intro_SCREEN:FindFirstChild("LoadScreen", true)
+            and playerGui.Intro_SCREEN.LoadScreen:FindFirstChild("Frame", true)
+            and playerGui.Intro_SCREEN.LoadScreen.Frame:FindFirstChild("Loaded")
+
+        if loadedCandidate and loadedCandidate:IsA("NumberValue") then
+            loadedValue = loadedCandidate
+            break
+        end
+        task.wait(0.3)
+    end
+
+    if not loadedValue then
+        warn("[AutoStart] Loaded объект не найден")
+        return
+    end
+
+    print("[AutoStart] Найден Loaded, отслеживаем значение...")
+
+    
+    while loadedValue.Value < 50 do
+        task.wait(0.1)
+    end
+
+    print("[AutoStart] Loaded > 50, эмуляция нажатия клавиши...")
+
+    
+    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Space, false, nil)
+    task.wait(0.1)
+    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Space, false, nil)
+end
+
+return AutoStart
+end end
+local Starter = __DARKLUA_BUNDLE_MODULES.load('a')
+
+Starter.AutoStart()
